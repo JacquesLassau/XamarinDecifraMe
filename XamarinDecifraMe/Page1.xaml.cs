385ef54c1696d3d50a020978499ff61a71a0f67c
JacquesLassau <37740292+JacquesLassau@users.noreply.github.com>
2020-08-25 08:16:48 -0300

    Conhecendo o Xamarin - App de navegação hierárquica

---
+++ XamarinDecifraMe/Page1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinDecifraMe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string texto = enResposta.Text.Trim();
+            if(texto.ToUpper() == "AZUL" || texto.ToUpper() == "AZÚL")
+            {
+                Navigation.PushAsync(new Page2());
+            }
+            else
+            {
+                DisplayAlert("A Esfinge Fala:", "Você foi devorado!", "OK");
+                Navigation.PopAsync();
+            }
+        }
+    }
+}
---
+++ XamarinDecifraMe/Page2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinDecifraMe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string texto = enResposta.Text.Trim();
+            if (texto.ToUpper() == "TRÊS" || texto.ToUpper() == "TRES" || texto == "3")
+            {
+                Navigation.PushAsync(new Page3());
+            }
+            else
+            {
+                DisplayAlert("A Esfinge Fala:", "Você foi devorado!", "OK");
+                Navigation.PopToRootAsync();
+            }
+        }
+    }
+}
